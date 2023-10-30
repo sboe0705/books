ac7f61dff70eaf679f935d5513dc15efb6158ad2
Sebastian Böhm <mail@sboe0705.de>
2023-10-30 07:36:40 +0100

    corrected docker compose scripts

--- src/test/resources/testdata.sql
+++ src/test/resources/testdata.sql
@@ -1,10 +1,10 @@
-INSERT INTO book (id, author, title) VALUES ((VALUES NEXT VALUE FOR book_seq), 'Michael Ende', 'Jim Knopf und Lukas der Lokomotivführer'); --1960
-INSERT INTO book (id, author, title) VALUES ((VALUES NEXT VALUE FOR book_seq), 'Michael Ende', 'Momo'); --1973
-INSERT INTO book (id, author, title) VALUES ((VALUES NEXT VALUE FOR book_seq), 'Michael Ende', 'Die Unendliche Geschichte'); --1979
-INSERT INTO book (id, author, title) VALUES ((VALUES NEXT VALUE FOR book_seq), 'Joanne K. Rowling', 'Harry Potter und der Stein der Weisen'); --1998
-INSERT INTO book (id, author, title) VALUES ((VALUES NEXT VALUE FOR book_seq), 'Joanne K. Rowling', 'Harry Potter und die Kammer des Schreckens'); --1999
-INSERT INTO book (id, author, title) VALUES ((VALUES NEXT VALUE FOR book_seq), 'Joanne K. Rowling', 'Harry Potter und der Gefangene von Askaban'); --1999
-INSERT INTO book (id, author, title) VALUES ((VALUES NEXT VALUE FOR book_seq), 'Joanne K. Rowling', 'Harry Potter und der Feuerkelch'); --2000
-INSERT INTO book (id, author, title) VALUES ((VALUES NEXT VALUE FOR book_seq), 'Joanne K. Rowling', 'Harry Potter und der Orden des Phönix'); --2003
-INSERT INTO book (id, author, title) VALUES ((VALUES NEXT VALUE FOR book_seq), 'Joanne K. Rowling', 'Harry Potter und der Halbblutprinz'); --2005
-INSERT INTO book (id, author, title) VALUES ((VALUES NEXT VALUE FOR book_seq), 'Joanne K. Rowling', 'Harry Potter und die Heiligtümer des Todes'); --2007
+INSERT INTO book (id, author, title) VALUES (1, 'Michael Ende', 'Jim Knopf und Lukas der Lokomotivführer'); --1960
+INSERT INTO book (id, author, title) VALUES (2, 'Michael Ende', 'Momo'); --1973
+INSERT INTO book (id, author, title) VALUES (3, 'Michael Ende', 'Die Unendliche Geschichte'); --1979
+INSERT INTO book (id, author, title) VALUES (4, 'Joanne K. Rowling', 'Harry Potter und der Stein der Weisen'); --1998
+INSERT INTO book (id, author, title) VALUES (5, 'Joanne K. Rowling', 'Harry Potter und die Kammer des Schreckens'); --1999
+INSERT INTO book (id, author, title) VALUES (6, 'Joanne K. Rowling', 'Harry Potter und der Gefangene von Askaban'); --1999
+INSERT INTO book (id, author, title) VALUES (7, 'Joanne K. Rowling', 'Harry Potter und der Feuerkelch'); --2000
+INSERT INTO book (id, author, title) VALUES (8, 'Joanne K. Rowling', 'Harry Potter und der Orden des Phönix'); --2003
+INSERT INTO book (id, author, title) VALUES (9, 'Joanne K. Rowling', 'Harry Potter und der Halbblutprinz'); --2005
+INSERT INTO book (id, author, title) VALUES (10, 'Joanne K. Rowling', 'Harry Potter und die Heiligtümer des Todes'); --2007
--- start-database-in-docker.sh
+++
@@ -1,2 +0,0 @@
-#/bin/bash
-docker run --name books-db --rm -p 5432:5432 -e POSTGRES_DB=books-db -e POSTGRES_USER=books-db-user -e POSTGRES_PASSWORD=books-db-password postgres:16-alpine
